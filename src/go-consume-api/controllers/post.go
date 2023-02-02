d6f0f34662c959b8b58eb4f7d620d6ece26d93e8
pjw1702 <pjw1702@hanmail.net>
2023-02-02 10:57:39 +0900

    consume API with Golang_modified

--- src/go-consume-api/controllers/post.go
+++ src/go-consume-api/controllers/post.go
@@ -14,6 +14,7 @@ import (
 )
 
 // Please refer to guide of jsonplaceholder
+// Get values matching keys by requesting HTTP method of Get
 type Post struct {
 	Id     int64  `json:"id"`
 	Title  string `json:"title"`
@@ -27,6 +28,7 @@ func Index(w http.ResponseWriter, r *http.Request) {
 
 	var posts []Post
 
+	// Get Rest API of https://jsonplaceholder.typicode.com
 	response, err := http.Get(BASE_URL + "/posts")
 	if err != nil {
 		log.Print(err)
@@ -34,6 +36,7 @@ func Index(w http.ResponseWriter, r *http.Request) {
 
 	defer response.Body.Close()
 
+	// Error handling by decoding data format of JSON Requested Get method
 	decoder := json.NewDecoder(response.Body)
 	if err := decoder.Decode(&posts); err != nil {
 		log.Print(err)
@@ -55,12 +58,14 @@ func Create(w http.ResponseWriter, r *http.Request) {
 
 	id := r.URL.Query().Get("id")
 	if id != "" {
+		// Error handling by requesting request HTTP method of Get
 		res, err := http.Get(BASE_URL + "/posts/" + id)
 		if err != nil {
 			log.Print(err)
 		}
 		defer res.Body.Close()
 
+		// Error handling by decoding data format of JSON by getting response of Get method
 		decoder := json.NewDecoder(res.Body)
 		if err := decoder.Decode(&post); err != nil {
 			log.Print(err)
@@ -115,6 +120,7 @@ func Store(w http.ResponseWriter, r *http.Request) {
 	// Set API Common Header
 	req.Header.Set("Content-Type", "application/json; charset=UTF-8")
 
+	// Create HTTP client
 	httpClient := &http.Client{}
 	res, err := httpClient.Do(req)
 	if err != nil {
@@ -124,6 +130,7 @@ func Store(w http.ResponseWriter, r *http.Request) {
 
 	var postsResponse Post
 
+	// Error handling by decoding data format of JSON Requested any method by the client
 	decoder := json.NewDecoder(res.Body)
 	if err := decoder.Decode(&postsResponse); err != nil {
 		log.Print(err)
@@ -143,12 +150,15 @@ func Delete(w http.ResponseWriter, r *http.Request) {
 
 	id := r.URL.Query().Get("id")
 
+	// Get http request of Delete by use id
 	req, err := http.NewRequest(http.MethodDelete, BASE_URL+"/posts/"+id, nil)
 	if err != nil {
 		log.Print(err)
 	}
 
+	// Create HTTP client
 	httpClient := &http.Client{}
+	// Request Delete to Rest API of https://jsonplaceholder.typicode.com
 	res, err := httpClient.Do(req)
 	if err != nil {
 		log.Print(err)
@@ -156,6 +166,7 @@ func Delete(w http.ResponseWriter, r *http.Request) {
 
 	defer res.Body.Close()
 
+	// Confirm HTTP status code by request
 	fmt.Println(res.StatusCode)
 	fmt.Println(res.Status)
 
